3be3f36cc89be603bdba41f0339e8712517d7775
Zhen Duan <zhen@arkade.com.au>
2022-10-14 08:07:25 +0800

    install img plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `my first gatsby app`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout title="Home Page">
       <div>
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        />
         <p>I'm making this by following the Gatsby Tutorial.</p>
         <Link to="/about">About Me</Link>
       </div>
